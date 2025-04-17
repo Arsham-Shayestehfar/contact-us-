2fd3a77e916c665d7076e2134ac510aa10815cfe
Arsham <arsham.sh9921@gmail.com>
2025-04-16 23:27:51 -0700

    add first needs and layout and views and models by master

---
+++ WebApplication13/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication13.Models
+{
+    public class Message
+    {
+        public string FullName { get; set; }
+        public string Subject { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
